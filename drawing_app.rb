2a3a1d1b280f54fdfa5ae13f85368f5dbc59534e
Oren Goldberg <ogoldberg@gmail.com>
2013-06-03 14:03:05 -0400

    started process of building interface and validation

--- drawing_app.rb
+++ drawing_app.rb
@@ -3,44 +3,89 @@ class DrawingApp
     attr_reader :rows
     attr_reader :graph
 
-    def initialize
+    ###########################
+    # I: INITIALIZE NEW GRAPH #
+    ###########################
 
+    def initialize
+        input = ["I"]
+        set_columns(input)
     end
 
-    def interface
-        print "Input text: "
-        input = gets.strip.upcase
-        take_action
+    def set_columns(input)
+        print "Let's make a graph! Please enter the number of columns:"
+        input << gets.strip.to_i
+        validate_columns(input)
+    end 
+
+    def set_rows(input)
+        puts "Please enter the number of rows (between 1 and 250):"
+        input << gets.strip.to_i
+        validate_rows(input)
     end
 
     def create_new_image(columns, rows)
-        @columns = columns
-        @rows = rows
         @graph = Array.new
         rows.times {@graph.push << []}
         @graph.each do |r|
            columns.times { r << "O"}
         end
         show
-   end
-
-    def input(command)
-        input_array = command.split(' ')
-        take_action(input_array)
+        puts "Nice! Now that we have a graph, we can play around with it. What would you like to do next?"
+        help
     end
+
+    ##################
+    # C: CLEAR TABLE #
+    ##################
 
     def clear_table
         @graph.each do |r|
             r.collect! { |p| p = "O"}
         end
         show
+        puts "Graph cleared"
+        help
     end
 
-    def color_pixel(x,y,c)
+    ####################
+    # L: COLOR A PIXEL #
+    ####################
+
+    def set_pixel_column
+        puts "Which column is the pixel in? 1-#{@graph[0].count.to_s}"
+        x = gets.strip.to_i
+        validate_pixel_column(x)
+    end
+
+    def set_pixel_row(x)
+        puts "Which row is the pixel in? 1-#{@graph.count.to_s}"
+        y = gets.strip.to_i
+        validate_pixel_row(x,y)
+    end
+
+    def set_pixel_color(x,y)
+        puts "What color should the pixel be? This can be any letter. Your letter will be automatically capitalized:"
+        c = gets.strip.upcase
+        validate_pixel_color(x,y,c)
+    end
+
+    def convert_input(x,y,c)
         x = x - 1
         y = y - 1
+        change_color(x,y,c)
+        show
+        help
+    end
+
+    def change_color(x,y,c)
         @graph[y][x] = c
     end
+
+
+    #########################
+    # DRAW VERTICAL SEGMENT #
+    #########################
 
     def draw_vertical(x,y1,y2,c)
         x  = x  - 1
@@ -57,6 +102,10 @@ class DrawingApp
         show
     end
 
+    #########################
+    # DRAW HORIZONTAL SEGMENT #
+    #########################
+
     def draw_horizontal(x1,x2,y,c)
         x1 = x1 - 1
         x2 = x2 - 1
@@ -71,6 +120,10 @@ class DrawingApp
         end
         show
     end
+
+    #########################
+    #       FILL TOOL       #
+    #########################
 
     def fill_region(x,y,c)
         oc = @graph[y][x]
@@ -134,9 +187,9 @@ class DrawingApp
         end
     end
 
-    def change_color(x,y,c)
-        @graph[y][x] = c
-    end
+    #########################
+    #         SHOW          #
+    #########################
 
     def show
         @graph.each do |r|
@@ -149,18 +202,20 @@ class DrawingApp
 
     end
 
+
 private
 
     def take_action(input)
         case input[0]
         when "I"
-            create_new_image(input[1].to_i, input[2].to_i)
+            validate_columns(input)
+            create_new_image(input[1], input[2])
         when "C"
             clear_table
         when "L"
-            color_pixel(input[1].to_i, input[2].to_i, input[3])
+            set_pixel_column
         when "V"
-           draw_vertical(input[1].to_i, input[2].to_i, input[3].to_i, input[4])
+            draw_vertical(input[1].to_i, input[2].to_i, input[3].to_i, input[4])
         when "H"
             draw_horizontal(input[1].to_i, input[2].to_i, input[3].to_i, input[4])
         when "F"
@@ -169,6 +224,89 @@ private
             show
         when "X"
             terminate_session
+        else
+            help
+        end
+    end
+
+    def help
+        puts "These are your options:
+        I M N. Create a new M x N image with all pixels colored white (O)
+        C. Clear the current image, setting all pixels to white (O).
+        L. Color a single pixel
+        V. Draw a vertical segment of color
+        H. Draw a horizontal segment of color
+        F. Fill a region
+        S. Show the contents of the current image
+        X. Terminate the session"
+        input = gets.strip.split(/ /)
+        input[0] = input[0].upcase
+        validate_command(input)
+    end
+    ###############
+    # VALIDATIONS #
+    ###############
+
+    def validate_command(input)
+        if input[0].length == 1
+            take_action(input)
+        else
+            puts "I didn't understand your entry. Please try again."
+            help
+        end
+    end
+
+    # create graph
+    def validate_columns(input)
+        input[1] = input[1].to_i
+        if input[1] > 0
+            validate_rows(input)
+        else
+            puts "To set the number of columns, please enter an integer greater than zero"
+            input[1] = gets.strip.to_i
+            validate_columns(input)
+        end
+    end
+
+    def validate_rows(input)
+        input[2] = input[2].to_i
+        if (1..250).include?(input[2])
+            puts input.inspect
+            create_new_image(input[1], input[2])
+        else
+            puts "To set the number of rows, please enter an integer between 1 and 250"
+            input[2] = gets.strip.to_i
+            validate_rows(input)
+        end
+    end
+
+    # color pixel
+    def validate_pixel_column(x)
+        if (1..@graph.count).include?(x)
+            set_pixel_row(x)
+        else
+            puts "You have entered an invalid column. Please try again."
+            set_pixel_column
+        end
+    end
+
+    def validate_pixel_row(x,y)
+        if (1..@graph[0].count).include?(y)
+            set_pixel_color(x,y,)
+        else
+            puts "You have entered an invalid row. Please try again."
+            set_pixel_row(x)
+        end
+    end
+
+    def validate_pixel_color(x,y,c)
+        if ("A".."Z").include?(c)
+            convert_input(x,y,c)
+        else
+            puts "You have entered an invalid color. Please try again."
+            set_pixel_color(x,y)
         end
     end
 end
+
+DrawingApp.new
---
+++ spec/drawing_spec.rb
@@ -0,0 +1,175 @@
+require "rspec"
+require_relative "../drawing_app"
+
+describe DrawingApp do
+    before(:each) do
+        @drawing_app = DrawingApp.new
+    end
+
+
+    describe "command interpreter" do 
+        it "should instantiate with 5 columns and 8 rows" do
+            @drawing_app.input("I 8 5")
+            @drawing_app.columns.should == 8
+            @drawing_app.rows.should == 5
+        end
+    end
+
+    describe "create graph" do
+        it "should have 40 pixels" do
+            @drawing_app.create_new_image(8,5)
+            @drawing_app.graph.count.should == 5
+            @drawing_app.graph.each do |c| 
+                c.count.should == 8
+            end
+        end
+    end
+
+    describe "clear table" do
+        it "should make all pixels white" do
+            @drawing_app.create_new_image(8,5)
+            @drawing_app.graph[4].map! {|p| p = "X"}
+            @drawing_app.clear_table
+            @drawing_app.graph.each do |r|
+                r.each do |p|
+                    p.should == "O"
+                end
+            end
+        end
+    end
+
+    describe "color pixel" do
+        it "should change the color of a specific pixel" do
+            @drawing_app.create_new_image(8,5)
+            @drawing_app.color_pixel(3,4,"Y")
+            @drawing_app.graph[3][2].should == "Y"
+            puts @drawing_app.graph[3][2].inspect
+            @drawing_app.graph[1][1].should_not == "Y"
+            puts @drawing_app.graph[1][1].inspect
+            @drawing_app.show
+        end
+    end
+
+    describe "draw_vertical" do
+        it "should draw a vertical line" do
+            @drawing_app.create_new_image(8,5)
+            @drawing_app.draw_vertical(2,1,4,"R")
+            rows =  [@drawing_app.graph[0], @drawing_app.graph[1], @drawing_app.graph[2], @drawing_app.graph[3]]
+            rows.each do |p|
+                p[1].should == "R"
+            end
+            @drawing_app.graph[0][0].should_not == "R"
+        end
+    end
+
+    describe "draw_horizontal" do
+        it "should draw a horizontal line with x1 < x2" do
+            @drawing_app.create_new_image(8,5)
+            @drawing_app.draw_horizontal(1,6,4,"G")
+
+            @drawing_app.graph[3][0..5].each do |p|
+                p.should == "G"
+            end
+            @drawing_app.graph[3][6].should_not == "G"
+        end
+
+        it "should draw a horizontal line with x1 > x2" do
+            @drawing_app.create_new_image(8,5)
+            @drawing_app.draw_horizontal(6,3,2,"G")
+
+            @drawing_app.graph[1][2..5].each do |p|
+                p.should == "G"
+            end
+            @drawing_app.graph[3][6].should_not == "G"
+        end
+    end
+
+    describe "fill tool" do
+        it "should fill a closed square" do
+            @drawing_app.create_new_image(20,20)
+            @drawing_app.draw_horizontal(4,15,4,"G")
+            @drawing_app.draw_horizontal(4,15,14,"G")
+            @drawing_app.draw_vertical(4,4,14, "G")
+            @drawing_app.draw_vertical(15,4,14, "G")
+            @drawing_app.color_pixel(7,8,"Y")
+            @drawing_app.fill_region(10,11, "B")
+            @drawing_app.graph[4][4..13].each do |p|
+                p.should == "B"
+            end
+            @drawing_app.graph[3][4..13].each do |p|
+                p.should == "G"
+            end
+        end
+
+        it "should leak out of an top right open cornered square" do
+            @drawing_app.create_new_image(20,20)
+            @drawing_app.draw_horizontal(4,14,4,"G")
+            @drawing_app.draw_horizontal(4,15,14,"G")
+            @drawing_app.draw_vertical(4,4,14, "G")
+            @drawing_app.draw_vertical(15,5,14, "G")
+            @drawing_app.color_pixel(7,8,"Y")
+            @drawing_app.fill_region(10,11, "B")
+            @drawing_app.graph[4][4..13].each do |p|
+                p.should == "B"
+            end
+            @drawing_app.graph[3][4..13].each do |p|
+                p.should == "G"
+            end
+        end
+
+        it "should leak out of a top left open cornered square" do
+            @drawing_app.create_new_image(20,20)
+            @drawing_app.draw_horizontal(5,15,4,"G")
+            @drawing_app.draw_horizontal(4,15,14,"G")
+            @drawing_app.draw_vertical(4,5,14, "G")
+            @drawing_app.draw_vertical(15,5,14, "G")
+            @drawing_app.color_pixel(7,8,"Y")
+            @drawing_app.fill_region(10,11, "B")
+            @drawing_app.graph[4][4..13].each do |p|
+                p.should == "B"
+            end
+            @drawing_app.graph[3][4..13].each do |p|
+                p.should == "G"
+            end
+        end
+
+        it "should leak out of a bottom left open cornered square" do
+            @drawing_app.create_new_image(20,20)
+            @drawing_app.draw_horizontal(4,15,4,"G")
+            @drawing_app.draw_horizontal(5,15,14,"G")
+            @drawing_app.draw_vertical(4,4,13, "G")
+            @drawing_app.draw_vertical(15,5,14, "G")
+            @drawing_app.color_pixel(7,8,"Y")
+            @drawing_app.fill_region(10,11, "B")
+            @drawing_app.graph[4][4..13].each do |p|
+                p.should == "B"
+            end
+            @drawing_app.graph[3][4..13].each do |p|
+                p.should == "G"
+            end
+        end
+
+        it "should leak out of a bottom right open cornered square" do
+            @drawing_app.create_new_image(20,20)
+            @drawing_app.draw_horizontal(4,15,4,"G")
+            @drawing_app.draw_horizontal(5,14,14,"G")
+            @drawing_app.draw_vertical(4,4,14, "G")
+            @drawing_app.draw_vertical(15,5,13, "G")
+            @drawing_app.color_pixel(7,8,"Y")
+            @drawing_app.fill_region(10,11, "B")
+            @drawing_app.graph[4][4..13].each do |p|
+                p.should == "B"
+            end
+            @drawing_app.graph[3][4..13].each do |p|
+                p.should == "G"
+            end
+        end
+    end
+
+end
+
+
+
+
+
+
