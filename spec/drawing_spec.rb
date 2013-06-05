fd9e31965679077f0b8e25dbd6c27ce497f01f3c
Oren Goldberg <ogoldberg@gmail.com>
2013-06-05 10:39:20 -0400

    fixed unit test issues.

--- spec/drawing_spec.rb
+++ spec/drawing_spec.rb
@@ -6,176 +6,112 @@ describe DrawingApp do
         @drawing_app = DrawingApp.new
     end
 
-
-    describe "command interpreter" do 
-        it "should instantiate with 5 columns and 8 rows" do
-            @drawing_app.create_new_image(8,5)
-            # @drawing_app.columns.should == 8
-            # @drawing_app.rows.should == 5
-        end
-    end
-
     describe "create graph" do
-        it "should have 40 pixels" do
-            @drawing_app.create_new_image(8,5)
-            @drawing_app.graph.count.should == 5
-            @drawing_app.graph.each do |c| 
-                c.count.should == 8
-            end
+        it "should have 12 pixels" do
+            @drawing_app.create_new_image(4,3)
+            expected_graph = [["O", "O", "O", "O"], ["O", "O", "O", "O"],["O", "O", "O", "O"]]
+            @drawing_app.graph.should == expected_graph
         end
     end
 
     describe "clear table" do
         it "should make all pixels white" do
-            @drawing_app.create_new_image(8,5)
-            @drawing_app.graph[4].map! {|p| p = "X"}
+            @drawing_app.create_new_image(4,3)
+            @drawing_app.graph[2].map! {|p| p = "X"}
             @drawing_app.clear_table
-            @drawing_app.graph.each do |r|
-                r.each do |p|
-                    p.should == "O"
-                end
-            end
+            expected_graph = [["O", "O", "O", "O"], ["O", "O", "O", "O"],["O", "O", "O", "O"]]
+            @drawing_app.graph.should == expected_graph
         end
     end
 
     describe "color pixel" do
         it "should change the color of a specific pixel" do
-            @drawing_app.create_new_image(8,5)
-            @drawing_app.color_pixel(3,4,"Y")
-            @drawing_app.graph[3][2].should == "Y"
-            puts @drawing_app.graph[3][2].inspect
-            @drawing_app.graph[1][1].should_not == "Y"
-            puts @drawing_app.graph[1][1].inspect
-            @drawing_app.show
+            @drawing_app.create_new_image(4,3)
+            @drawing_app.color_pixel(3,2,"Y")
+            expected_graph = [["O", "O", "O", "O"], ["O", "O", "Y", "O"],["O", "O", "O", "O"]]
+            @drawing_app.graph.should == expected_graph
         end
     end
 
     describe "draw_vertical" do
         it "should draw a vertical line" do
-            @drawing_app.create_new_image(8,5)
-            @drawing_app.draw_vertical(2,1,4,"R")
-            rows =  [@drawing_app.graph[0], @drawing_app.graph[1], @drawing_app.graph[2], @drawing_app.graph[3]]
-            rows.each do |p|
-                p[1].should == "R"
-            end
-            @drawing_app.graph[0][0].should_not == "R"
+            @drawing_app.create_new_image(4,3)
+            @drawing_app.draw_vertical(2,1,3,"R")
+            expected_graph = [["O", "R", "O", "O"], ["O", "R", "O", "O"],["O", "R", "O", "O"]]
+            @drawing_app.graph.should == expected_graph
         end
     end
 
     describe "draw_horizontal" do
         it "should draw a horizontal line with x1 < x2" do
-            @drawing_app.create_new_image(8,5)
-            @drawing_app.draw_horizontal(1,6,4,"G")
-
-            @drawing_app.graph[3][0..5].each do |p|
-                p.should == "G"
-            end
-            @drawing_app.graph[3][6].should_not == "G"
+            @drawing_app.create_new_image(4,3)
+            @drawing_app.draw_horizontal(3,1,3,"G")
+            expected_graph = [["O", "O", "O", "O"], ["O", "O", "O", "O"],["G", "G", "G", "O"]]
+            @drawing_app.graph.should == expected_graph
         end
 
         it "should draw a horizontal line with x1 > x2" do
-            @drawing_app.create_new_image(8,5)
-            @drawing_app.draw_horizontal(6,3,2,"G")
-
-            @drawing_app.graph[1][2..5].each do |p|
-                p.should == "G"
-            end
-            @drawing_app.graph[3][6].should_not == "G"
+            @drawing_app.create_new_image(4,3)
+            @drawing_app.draw_horizontal(1,3,3,"G")
+            expected_graph = [["O", "O", "O", "O"], ["O", "O", "O", "O"],["G", "G", "G", "O"]]
+            @drawing_app.graph.should == expected_graph
         end
     end
 
     describe "fill tool" do
         it "should fill a closed square" do
-            @drawing_app.create_new_image(20,20)
-            @drawing_app.draw_horizontal(4,15,4,"G")
-            @drawing_app.draw_horizontal(4,15,14,"G")
-            @drawing_app.draw_vertical(4,4,14, "G")
-            @drawing_app.draw_vertical(15,4,14, "G")
-            @drawing_app.color_pixel(7,8,"Y")
-            @drawing_app.fill_region(10,11, "B")
-            @drawing_app.graph[4][4..13].each do |p|
-                p.should == "B"
-            end
-            @drawing_app.graph[3][4..13].each do |p|
-                p.should == "G"
-            end
-        end
-
-        it "should leak out of an top right open cornered square" do
-            @drawing_app.create_new_image(20,20)
-            @drawing_app.draw_horizontal(4,14,4,"G")
-            @drawing_app.draw_horizontal(4,15,14,"G")
-            @drawing_app.draw_vertical(4,4,14, "G")
-            @drawing_app.draw_vertical(15,5,14, "G")
-            @drawing_app.color_pixel(7,8,"Y")
-            @drawing_app.fill_region(10,11, "B")
-            @drawing_app.graph[4][4..13].each do |p|
-                p.should == "B"
-            end
-            @drawing_app.graph[3][4..13].each do |p|
-                p.should == "G"
-            end
-        end
-
-        it "should leak out of a top left open cornered square" do
-            @drawing_app.create_new_image(20,20)
-            @drawing_app.draw_horizontal(5,15,4,"G")
-            @drawing_app.draw_horizontal(4,15,14,"G")
-            @drawing_app.draw_vertical(4,5,14, "G")
-            @drawing_app.draw_vertical(15,5,14, "G")
-            @drawing_app.color_pixel(7,8,"Y")
-            @drawing_app.fill_region(10,11, "B")
-            @drawing_app.graph[4][4..13].each do |p|
-                p.should == "B"
-            end
-            @drawing_app.graph[3][4..13].each do |p|
-                p.should == "G"
-            end
-        end
-
-        it "should leak out of a bottom left open cornered square" do
-            @drawing_app.create_new_image(20,20)
-            @drawing_app.draw_horizontal(4,15,4,"G")
-            @drawing_app.draw_horizontal(5,15,14,"G")
-            @drawing_app.draw_vertical(4,4,13, "G")
-            @drawing_app.draw_vertical(15,5,14, "G")
-            @drawing_app.color_pixel(7,8,"Y")
-            @drawing_app.fill_region(10,11, "B")
-            @drawing_app.graph[4][4..13].each do |p|
-                p.should == "B"
-            end
-            @drawing_app.graph[3][4..13].each do |p|
-                p.should == "G"
-            end
-        end
-
-        it "should leak out of a bottom right open cornered square" do
-            @drawing_app.create_new_image(20,20)
-            @drawing_app.draw_horizontal(4,15,4,"G")
-            @drawing_app.draw_horizontal(5,14,14,"G")
-            @drawing_app.draw_vertical(4,4,14, "G")
-            @drawing_app.draw_vertical(15,5,13, "G")
-            @drawing_app.color_pixel(7,8,"Y")
-            @drawing_app.fill_region(10,11, "B")
-            @drawing_app.graph[4][4..13].each do |p|
-                p.should == "B"
-            end
-            @drawing_app.graph[3][4..13].each do |p|
-                p.should == "G"
-            end
-        end
-
-        it "should legitimately fill a closed square" do
             @drawing_app.create_new_image(3,3)
-            expected_graph = [["C", "C", "C"], ["C", "B", "C"],["C", "B", "C"]]
             @drawing_app.graph[0] = ["C", "C", "C"]
             @drawing_app.graph[1] = ["C", "A", "C"]
             @drawing_app.graph[2] = ["C", "A", "C"]
-            puts @drawing_app.graph.inspect
             @drawing_app.fill_region(2, 2, "B")
+            expected_graph = [["C", "C", "C"], ["C", "B", "C"],["C", "B", "C"]]
             @drawing_app.graph.should == expected_graph
         end
 
+        it "should leak out of a top right open cornered square" do
+            @drawing_app.create_new_image(4,4)
+            @drawing_app.graph[0] = ["C", "C", "C", "C"]
+            @drawing_app.graph[1] = ["M", "M", "C", "C"]
+            @drawing_app.graph[2] = ["M", "C", "M", "C"]
+            @drawing_app.graph[3] = ["M", "M", "M", "C"]
+            @drawing_app.fill_region(2, 3, "B")
+            expected_graph = [["B", "B", "B", "B"], ["M", "M", "B", "B"],["M", "B", "M", "B"],["M", "M", "M", "B"]]
+            @drawing_app.graph.should == expected_graph
+        end
+
+        it "should leak out of a top left open cornered square" do
+            @drawing_app.create_new_image(4,4)
+            @drawing_app.graph[0] = ["C", "C", "C", "C"]
+            @drawing_app.graph[1] = ["C", "M", "M", "C"]
+            @drawing_app.graph[2] = ["M", "C", "M", "C"]
+            @drawing_app.graph[3] = ["M", "M", "M", "C"]
+            @drawing_app.fill_region(2, 3, "B")
+            expected_graph = [["B", "B", "B", "B"], ["B", "M", "M", "B"],["M", "B", "M", "B"],["M", "M", "M", "B"]]
+            @drawing_app.graph.should == expected_graph
+        end
+
+        it "should leak out of a bottom left open cornered square" do
+            @drawing_app.create_new_image(4,4)
+            @drawing_app.graph[0] = ["C", "C", "C", "C"]
+            @drawing_app.graph[1] = ["C", "M", "M", "M"]
+            @drawing_app.graph[2] = ["C", "M", "C", "M"]
+            @drawing_app.graph[3] = ["C", "C", "M", "M"]
+            @drawing_app.fill_region(3, 3, "B")
+            expected_graph = [["B", "B", "B", "B"], ["B", "M", "M", "M"],["B", "M", "B", "M"],["B", "B", "M", "M"]]
+            @drawing_app.graph.should == expected_graph            
+        end
+
+        it "should leak out of a bottom right open cornered square" do
+            @drawing_app.create_new_image(4,4)
+            @drawing_app.graph[0] = ["C", "C", "C", "C"]
+            @drawing_app.graph[1] = ["M", "M", "M", "C"]
+            @drawing_app.graph[2] = ["M", "C", "M", "C"]
+            @drawing_app.graph[3] = ["M", "M", "C", "C"]
+            @drawing_app.fill_region(2, 3, "B")
+            expected_graph = [["B", "B", "B", "B"], ["M", "M", "M", "B"],["M", "B", "M", "B"],["M", "M", "B", "B"]]
+            @drawing_app.graph.should == expected_graph
+        end
     end
 
 end
