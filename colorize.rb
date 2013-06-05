96f09e4329093cb16fc91b9e73ac976722dbc162
Oren Goldberg <ogoldberg@gmail.com>
2013-06-05 15:40:29 -0400

    colorize file

---
+++ colorize.rb
@@ -0,0 +1,26 @@
+class String
+    # colorization
+    def colorize(color_code)
+        "\e[#{color_code}m#{self}\e[0m"
+    end
+
+    def red
+        colorize(31)
+    end
+
+    def green
+        colorize(32)
+    end
+
+    def yellow
+        colorize(33)
+    end
+
+    def pink
+        colorize(35)
+    end
+
+    def white
+        colorize(47)
+    end
+end
